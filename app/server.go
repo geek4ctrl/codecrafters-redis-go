60623cad5dbf26007a90b841f2bb489ca279d9a6
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-03-16 13:48:27 +0200

    Respond to PING

--- app/server.go
+++ app/server.go
@@ -14,13 +14,20 @@ func main() {
 	// Uncomment this block to pass the first stage
 	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	_, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	if _ {
+		fmt.Println("+OK\r\n")
 		os.Exit(1)
 	}
 }
